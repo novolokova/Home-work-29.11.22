60790e1f37ecc2534b0844bd254e4d58c427a75a
Margarita <m.novolokova@gmail.com>
2022-11-29 21:40:08 +0200

    Refactor code

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function toJadenCase(str) {
     .trim()
     .toLowerCase()
     .split(" ")
-    .filter((word) => word !== "")
-    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .filter(word => word !== "")
+    .map(word => word[0].toUpperCase() + word.substring(1))
     .join(" ");
 }
 
 console.log(toJadenCase(sentence));
+
