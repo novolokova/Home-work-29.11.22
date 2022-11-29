2184410de57b4ab71b6a7010683e027b671f5593
Margarita <m.novolokova@gmail.com>
2022-11-29 14:22:20 +0200

    Task - toJadenCase

--- index.js
+++ index.js
@@ -3,17 +3,22 @@
 // Виправити помилки в коді
 //повинен бути результат:  'To Be Or Not To Be'
 
-
 const sentence = "     to         BE       oR   nOt        To     bE ";
 
+/**
+ * returns the string, removes extra spaces, returns each word in uppercase
+ * 
+ * @param {string} str 
+ * @returns {string}
+ */
 function toJadenCase(str) {
-return str
-.trim()
-.toLowerCase()
-.split(" ")
-.map((word) => word[0].toUpperCase() + word.substring(1))
-.join(" ");
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .filter((word) => word !== "")
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
 }
-console.log(toJadenCase(sentence)); 
 
-
+console.log(toJadenCase(sentence));
